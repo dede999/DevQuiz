565b92064f982eca768a374dbd61ab6d142cfcf6
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2021-04-27 20:00:27 -0300

    Fix score card style

--- lib/home/widgets/score-card-widget.dart
+++ lib/home/widgets/score-card-widget.dart
@@ -13,29 +13,32 @@ class ScoreCardWidget extends StatelessWidget {
               color: AppColors.white, borderRadius: BorderRadius.circular(10)),
           child: Padding(
             padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: ChartWidget(),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Vamos começar, ", style: AppTextStyles.heading),
-                        Text("Complete os desafios e avançe em conhecimento",
-                            style: AppTextStyles.body)
-                      ],
-                    ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: ChartWidget(),
                   ),
-                )
-              ],
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Vamos começar, ", style: AppTextStyles.heading),
+                          Text("Complete os desafios e avançe em conhecimento",
+                              style: AppTextStyles.body)
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           )),
     );
