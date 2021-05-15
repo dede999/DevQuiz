c4c13b515b5694ffb482f6cbb17ffb0f6d2359e5
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2021-05-15 13:57:45 -0300

    Add Quiz Card widget

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dev_quiz/home/widgets/app_bar.dart';
 import 'package:dev_quiz/home/widgets/level-button-widget.dart';
+import 'package:dev_quiz/home/widgets/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,17 +15,35 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidgets(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-        child: Row(
-          children: [
-            LevelButtonWidget(label: 'Fácil'),
-            LevelButtonWidget(label: 'Médio'),
-            LevelButtonWidget(label: 'Dificil'),
-            LevelButtonWidget(label: 'Perito')
-          ],
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15, top: 15, right: 15),
+            child: Row(
+              children: [
+                LevelButtonWidget(label: 'Fácil'),
+                LevelButtonWidget(label: 'Médio'),
+                LevelButtonWidget(label: 'Dificil'),
+                LevelButtonWidget(label: 'Perito')
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget()
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/home/widgets/quiz_card_widget.dart
@@ -0,0 +1,44 @@
+import 'package:dev_quiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  // const QuizCardWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+          border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              height: 40, width: 40, child: Image.asset(AppImages.blocks)),
+          SizedBox(height: 15),
+          Text('Gerenciamento de Estados', style: AppTextStyles.heading15),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                  flex: 1, child: Text('3/10', style: AppTextStyles.body11)),
+              Expanded(
+                flex: 4,
+                child: LinearProgressIndicator(
+                  value: 0.3,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
